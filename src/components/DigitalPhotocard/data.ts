83e0f92db62b8dc05365dd29cf489719bfe6a638
ibang090202 <ialamsyah92@gmail.com>
2026-06-15 18:42:54 +0700

    update itinerary items and remove time field

--- src/components/DigitalPhotocard/data.ts
+++ src/components/DigitalPhotocard/data.ts
@@ -2,33 +2,56 @@ export const GIRLFRIEND_NAME = "Rahma Sani";
 export const BIRTHDAY_DATE = "16061996";
 export const BIRTHDAY_AGE = "30th";
 
+// export const ITINERARY = [
+//   {
+//     id: 1,
+//     theme: "Soundcheck (Morning Coffee)",
+//     name: "Kopi Selasar Sunaryo",
+//     desc: "Tempat yang tenang untuk memulai hari."
+//   },
+//   {
+//     id: 2,
+//     theme: "Main Stage (City Walk)",
+//     name: "Grey Art Gallery",
+//     desc: "Menikmati pameran seni di area pusat kota."
+//   },
+//   {
+//     id: 3,
+//     theme: "Special VCR (Activity)",
+//     name: "The Portrait Place",
+//     desc: "Photobox biar kita bisa bikin pose ala-ala."
+//   },
+//   {
+//     id: 4,
+//     theme: "Encore Stage (Dinner)",
+//     name: "Brussels Spring",
+//     desc: "Makan malam romantis sebagai penutup yang sempurna."
+//   },
+// ];
+
 export const ITINERARY = [
   {
     id: 1,
-    time: "10:00",
-    theme: "Soundcheck (Morning Coffee)",
+    theme: "Soundcheck (Coffee & Art Space)",
     name: "Kopi Selasar Sunaryo",
-    desc: "Tempat yang tenang untuk memulai hari."
+    desc: "Ngopi pagi santai sambil melihat pameran seni."
   },
   {
     id: 2,
-    time: "13:00",
-    theme: "Main Stage (City Walk)",
-    name: "Grey Art Gallery",
-    desc: "Menikmati pameran seni di area pusat kota."
+    theme: "Main Stage (The Actor's Screen)",
+    name: "Nonton Film 'Colony'",
+    desc: "Movie date seru nonton film Colony."
   },
   {
     id: 3,
-    time: "16:00",
     theme: "Special VCR (Activity)",
     name: "The Portrait Place",
-    desc: "Photobox biar kita bisa bikin pose ala-ala."
+    desc: "Sesi photobox berdua pakai tiket VIP."
   },
   {
     id: 4,
-    time: "19:00",
     theme: "Encore Stage (Dinner)",
     name: "Brussels Spring",
-    desc: "Makan malam romantis sebagai penutup yang sempurna."
-  },
+    desc: "Makan malam romantis sebagai penutup konser."
+  }
 ];
